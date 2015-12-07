e8da51b7a03184768979aac5251dc706af1b76df
tumayun <tumayun.2010@gmail.com>
2015-12-07 13:26:20 +0800

    Add goliath.pill to linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, "/u/apps/#{fetch(:application)}"
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, ['.env', 'config/newrelic.yml', '.ruby-version'])
+set :linked_files, fetch(:linked_files, ['.env', 'config/newrelic.yml', '.ruby-version', 'goliath.pill'])
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets')
