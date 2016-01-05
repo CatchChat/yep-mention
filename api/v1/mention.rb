ec7b9cb74eaeaefccf3a6e8c509c9b22593d8bb3
tumayun <tumayun.2010@gmail.com>
2016-01-05 22:50:37 +0800

    Improve mention response

--- api/v1/mention.rb
+++ api/v1/mention.rb
@@ -19,19 +19,17 @@ module Yep
     "prefix":{
       "username":"#{lowercased_word}"
     }
-  },
-  "sort":{
-    "last_sign_in_at":"desc"
   }
 }
         STR
         req = EM::HttpRequest.new("#{ENV['ELASTICSEARCH_URL']}/users/user/_search?form=0&size=5&source=#{CGI.escape(source)}").get
         result = ActiveSupport::JSON.decode req.response
+        hits = result['hits']['hits'] rescue []
 
-        result['hits']['hits'].map do |user|
+        hits.map do |user|
           {
             id: AES256.encrypt_id(user['_id']),
-            avatar_url: user['_source']['avatar_url'],
+            avatar: { thumb_url: user['_source']['avatar'] ? "https://s3.cn-north-1.amazonaws.com.cn/#{ENV['AWS_AVATARS_BUCKET']}/#{user['_source']['avatar']}" : nil },
             username: user['_source']['username'],
             nickname: user['_source']['nickname']
           }
